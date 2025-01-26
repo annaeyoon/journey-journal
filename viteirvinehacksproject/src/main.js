4d9453e76b009be423e1987973be733d5f9590af
Anna Yoon <annaeyoon9@gmail.com>
2025-01-25 22:57:24 -0800

    edit

--- viteirvinehacksproject/src/main.js
+++ viteirvinehacksproject/src/main.js
@@ -1,46 +1,45 @@
 import Groq from 'groq-sdk';
-import mysql from 'mysql2';
 
 // Create a new instance of the Groq class
 const groq = new Groq({ apiKey: import.meta.env.VITE_GROQ_API_KEY, dangerouslyAllowBrowser: true });
   
-function jsonItinerary(json) {
-  const db = mysql.createConnection({
-      host: 'database-irvine.cn2ymqgeihdj.us-east-1.rds.amazonaws.com',
-      user: 'admin',        // your MySQL username
-      password: 'adminpw!', // your MySQL password
-      database: 'irvinedb'        // your database name
-  });
+// function jsonItinerary(json) {
+//   const db = mysql.createConnection({
+//       host: 'database-irvine.cn2ymqgeihdj.us-east-1.rds.amazonaws.com',
+//       user: 'admin',        // your MySQL username
+//       password: 'adminpw!', // your MySQL password
+//       database: 'irvinedb'        // your database name
+//   });
 
-  db.connect(err => {
-      if (err) {
-      console.log('Error connecting to the database:', err);
-      return;
-      }
-      console.log('Connected to the MySQL database');
-  });
+//   db.connect(err => {
+//       if (err) {
+//       console.log('Error connecting to the database:', err);
+//       return;
+//       }
+//       console.log('Connected to the MySQL database');
+//   });
 
-  const query = 'INSERT INTO users_itinerary (user_id, itinerary) VALUES (?, ?)';
-  const userId = sessionStorage.getItem('userid');
+//   const query = 'INSERT INTO users_itinerary (user_id, itinerary) VALUES (?, ?)';
+//   const userId = sessionStorage.getItem('userid');
 
-  // Execute the query
-  db.query(query, [userId, JSON.stringify(json)], (err, results) => {
-      if (err) {
-          console.error('Error inserting data into the database:', err);
-          return;
-      }
-      console.log('Itinerary saved successfully:', results);
-  });
+//   // Execute the query
+//   db.query(query, [userId, JSON.stringify(json)], (err, results) => {
+//       if (err) {
+//           console.error('Error inserting data into the database:', err);
+//           return;
+//       }
+//       console.log('Itinerary saved successfully:', results);
+//   });
 
-  // Close the database connection
-  db.end(err => {
-      if (err) {
-          console.error('Error closing the database connection:', err);
-          return;
-      }
-      console.log('Database connection closed');
-  });
-}
+//   // Close the database connection
+//   db.end(err => {
+//       if (err) {
+//           console.error('Error closing the database connection:', err);
+//           return;
+//       }
+//       console.log('Database connection closed');
+//   });
+// }
 // Function to get chat completion
 export async function chat(input, conversationContext) {
     if (input.toLowerCase() === "exit") {
