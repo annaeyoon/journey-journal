037b84ce37d4b9c8c254cc4a21d1625a6145bb09
Anna Yoon <annaeyoon9@gmail.com>
2025-01-25 22:12:39 -0800

    login

--- viteirvinehacksproject/src/login.js
+++ viteirvinehacksproject/src/login.js
@@ -6,3 +6,29 @@ function getUsername() {
 }
 
 document.getElementById("buttonID").addEventListener("click", getUsername);
+
+import express from 'express';
+import mysql from 'mysql2';
+
+
+function connectToDB() {
+    const db = mysql.createConnection({
+        host: 'database-irvine.cn2ymqgeihdj.us-east-1.rds.amazonaws.com',
+        user: 'admin',        // your MySQL username
+        password: 'adminpw!', // your MySQL password
+        database: 'irvinedb'        // your database name
+    });
+
+    db.connect(err => {
+        if (err) {
+        console.log('Error connecting to the database:', err);
+        return;
+        }
+        console.log('Connected to the MySQL database');
+    });
+
+    db.query
+
+
+}
+
